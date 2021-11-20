a0b915b4038440b34b2f086aab21e68c8f2011e0
Moch. Fajar Hikmatulloh <93021746+mochfajarh@users.noreply.github.com>
2021-11-20 11:11:56 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,14 @@
+package jawabanuts;
+public vlass jawaban {
+  public static void main(String[] args) {
+    String salam = "Assalamualaiakum";
+    String nama = "Moch Fajar Hikmatulloh";
+    int tahunlahir = 1998;
+    
+    
+    System.out.println(salam);
+    System.out.println("Nama saya"+nama);
+    System.out.println("Saya Lahir ditahun",+tahunlahir);
+    
+  }
+}
